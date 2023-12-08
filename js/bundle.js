1537e048a3409fc6d35c698a8800beaa71b0b6e1
pegasus-lynx <pegasus-lynx@users.noreply.github.com>
2023-12-08 06:31:16 +0000

    deploy: 5d7cedf245633ec9f14b9a9045437d6e5af57705

---
+++ js/bundle.js
@@ -0,0 +1,17 @@
+(() => {
+  // src/assets/js/index.js
+  function copyPageUrl() {
+    const ta = document.createElement("textarea");
+    ta.value = window.location.href;
+    ta.setAttribute("readonly", "");
+    ta.style.position = "absolute";
+    ta.style.left = "-9999px";
+    document.body.appendChild(ta);
+    ta.select();
+    ta.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(ta);
+  }
+  globalThis.App = { copyPageUrl };
+})();
+//# sourceMappingURL=bundle.js.map
